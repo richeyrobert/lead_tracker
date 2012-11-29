97aebd6a32dcd65f1bf4e52cb9be8b2c6d74f57e
richeyrobert <rob@rocketnext.com>
2012-11-28 19:24:32 -0700

    working jquery!

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -18,6 +18,7 @@ class LeadsController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @lead }
+      format.js
     end
   end
 
